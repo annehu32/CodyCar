8d74ae8a52cf772f302b63e91389e45191e2b4c9
Anne Hu <anne.hu@tufts.edu>
2025-02-09 09:29:28 -0500

    Update carController.py
    Sending control messages to esp

--- carController.py
+++ carController.py
@@ -5,14 +5,23 @@ from BLE_CEEO import Yell
 from machine import Pin
 
 def peripheral(name):
+    # Defining GPIO pin assignments
+    button = Pin('GPIO0', Pin.IN, Pin.PULL_UP)
+    
     try:
         # Creating the yelling component
         coms = Yell('pico', verbose = True)
+        if coms.connect_up():
+            print("Coms is connected via Bluetooth")
+
         while True:
-            if coms.connect_up():
-                print("Coms is connected via Bluetooth")
-                time.sleep(1)
-                coms.send("testing from Pico")
+            if(not button.value()): # Upon button press:
+                print("Button has been pressed!")
+                #message = readSensors() # TODO: Write sensor reading program
+                message = 'FLBR' # tester message while waiting for sensor code
+                if coms.connect_up():
+                        time.sleep(0.01)
+                        coms.send(message)
             
     except Exception as e:
         print("except 1")
